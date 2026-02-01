443fea0dd5cc316e27e5c4c7ff9d72e0b40c171c
Andrew Paetznick <paetz045@umn.edu>
2026-02-01 17:04:00 -0600

    Basic EFuse logic and framework

--- Core/Inc/Efuse.h
+++ Core/Inc/Efuse.h
@@ -1,1 +1,37 @@
 // Set up header file for Efuse with function prorotypes and variables as needed
+
+#ifndef INC_Efuse_H_
+#define INC_Efuse_H_
+
+#include "main.h"
+#include "GopherCAN.h"
+
+//states of efuse
+#define ENABLED 0
+#define TRIPPED 1
+#define SHUTDOWN 2
+
+
+typedef struct {
+    FLOAT_CAN_STRUCT* parameter;
+    GPIO_TypeDef* enable_fuse_port;
+    uint16_t enable_fuse_pin;
+    GPIO_TypeDef* flt_out_port;
+    uint16_t flt_out_pin;
+    GPIO_TypeDef* flt_led_port;
+    uint16_t flt_led_pin;
+    ADC_TypeDef* curr_out_port;
+    uint16_t curr_out_pin;
+    float curr_limit = 0.0;
+    uint8_t flt;
+    uint32_t reset_delay_ms;
+    uint8_t state;
+    uint32_t last_update;
+    uint8_t flt_count;
+    uint8_t max_flt_count;                      //amount of retries before disabling
+    U8_CAN_STRUCT* overcurrentparam;
+    U8_CAN_STRUCT* overcurrentcountparam;
+} EFUSE_DATA;
+
+
+#endif /* INC_Efuse_H_ */
--- Core/Src/Efuse.c
+++ Core/Src/Efuse.c
@@ -1,1 +1,101 @@
 // TODO: Write an Efuse Function with two states
+#include "EFuse.h"
+
+uint8_t SNS_5V_1_FLT;
+uint8_t SNS_5V_2_FLT;
+uint8_t SNS_12V_FLT;
+
+//Basic 5V Efuse setup
+EFUSE_DATA* SNS_5V_1 = {
+    .enable_fuse_port = SNS_5V_1_EN_GPIO_PORT;
+    .enable_fuse_pin = SNS_5V_1_EN_PIN;
+    .flt_out_port = SNS_5V_1_FLT_GPIO_PORT;
+    .flt_out_pin = SNS_5V_1_FLT_PIN;
+    .flt_led_port = FLT_5V_1_LED_GPIO_PORT;
+    .flt_led_pin = FLT_5V_1_LED_PIN;
+    .flt = 0;
+    .reset_delay_ms = 1000;
+    .state = ENABLED;
+    .last_update = 0;
+    .flt_count = 0;
+    .max_flt_count = 3;
+} ;
+
+EFUSE_DATA* SNS_5V_1 = {
+    .enable_fuse_port = SNS_5V_2_EN_GPIO_PORT;
+    .enable_fuse_pin = SNS_5V_2_EN_PIN;
+    .flt_out_port = SNS_5V_2_FLT_GPIO_PORT;
+    .flt_out_pin = SNS_5V_2_FLT_PIN;
+    .flt_led_port = FLT_5V_2_LED_GPIO_PORT;
+    .flt_led_pin = FLT_5V_2_LED_PIN;
+    .flt = 0;
+    .reset_delay_ms = 1000;
+    .state = ENABLED;
+    .last_update = 0;
+    .flt_count = 0;
+    .max_flt_count = 3;
+} ;
+
+EFUSE_DATA* SNS_12V = {
+    .enable_fuse_port = SNS_12V_EN_GPIO_PORT;
+    .enable_fuse_pin = SNS_12V_EN_PIN;
+    .flt_out_port = SNS_12V_FLT_GPIO_PORT;
+    .flt_out_pin = SNS_12V_FLT_PIN;
+    .flt_led_port = FLT_12V_LED_GPIO_PORT;
+    .flt_led_pin = FLT_12V_LED_PIN;
+    .curr_out_port = SNS_12V_ILM_GPIO_Port;
+    .curr_out_pin = SNS_12V_ILM_PIN;
+    .curr_limit = 0.6;
+    .flt = 0;
+    .reset_delay_ms = 1000;
+    .state = ENABLED;
+    .last_update = 0;
+    .flt_count = 0;
+    .max_flt_count = 3;
+} ;
+
+
+void update_efuse(EFUSE_DATA* efuse) {
+    efuse.flt = HAL_GPIO_ReadPin(efuse.flt_out_port, efuse.flt_pin);
+    uint32_t tick = HAL_GetTick();
+
+    //flips to tripped if enabled and flt asserted
+    if(efuse.flt && (efuse.state == ENABLED)){
+        efuse.fltcount ++;
+        efuse.state = TRIPPED;
+        HAL_GPIO_WritePin(efuse.enable_fuse_port, efuse.enable_fuse_pin, 0);
+        HAL_GPIO_WritePin(efuse.flt_led_port, efuse.flt_led_pin, 0);
+        efuse.last_update = tick;
+    } 
+
+    //ilm current is checked for 12V, flt only asserts for ilm-gnd short and overtemp
+    if((efuse.curr_limit != 0) && (efuse.state == ENABLED)){
+        if(12VElectronicFuseCurrent_mamps.data > 450){
+            efuse.fltcount ++;
+            efuse.state = TRIPPED;
+            HAL_GPIO_WritePin(efuse.enable_fuse_port, efuse.enable_fuse_pin, 0);
+            HAL_GPIO_WritePin(efuse.flt_led_port, efuse.flt_led_pin, 0);
+            efuse.last_update = tick;
+        } else{
+            efuse.last_update = tick;
+        }
+    }
+
+    //if in tripped state and delay has finished
+    if((efuse.state == TRIPPED) && (tick - efuse.last_update > efuse.reset_delay_ms)){
+        if(efuse.flt){
+            efuse.fltcount ++;
+        } else{ //flips back leds
+            efuse.state = ENABLED;
+            HAL_GPIO_WritePin(efuse.enable_fuse_port, efuse.enable_fuse_pin, 1);
+            HAL_GPIO_WritePin(efuse.flt_led_port, efuse.flt_led_pin, 1);
+        }
+        efuse.last_update = tick;
+    }
+
+    //shutdown catch
+    if(!(efuse.state == SHUTDOWN) && (efuse.flt_cout >= efuse.max_flt_count)){
+        efuse.state == SHUTDOWN;
+        efuse.last_update = tick;
+    }
+}
